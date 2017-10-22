86195ce5f8911367922e69e134342307767ee791
Echo <jiechenlei36@gmail.com>
2017-10-22 16:03:59 +0100

    Create UploadPlan.cs

---
+++ tests/UploadPlan.cs
@@ -0,0 +1,34 @@
+.container_s {
+    width: 1000px;
+    margin-left:50px;
+}
+
+#header {
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    height:100px;
+    position: absolute;
+    z-index: 301;
+    min-width: 1000px;
+    background: #fff;
+    border-bottom: 1px solid #ebebeb;
+    position: fixed;
+}
+
+#content_right {
+    border-left: 1px solid #e1e1e1;
+    width: 750px;
+    height:800px;
+    float: right;
+    padding-top: 100px;
+}
+#content_left {
+    width: 200px;
+    height:800px;
+    float: left;
+    padding-top: 100px;
+}
+div {
+    display: block;
+}
