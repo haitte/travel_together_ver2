b8e7af22f749a2ce526076b0dab1b2f0bbaf2964
Sana Diwan <sanadiwan2006@gmail.com>
2017-11-21 13:50:20 +0000

    db migration and surveycontroller

---
+++ database/migrations/2017_11_21_132453_create_surveys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->increments('survey_id');
+            $table->integer('trip_id');
+            $table->string('first_p');
+            $table->string('second_p');
+            $table->string('third_p');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+}
