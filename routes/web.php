842db7ab1a5b6282313a1341f3f3e100640afc25
x01318877 <jiechenlei36@gmail.com>
2017-12-06 11:31:37 +0000

    sixth try

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/publishPlan', 'PublishPlanController@index')->name('publishPlan');
 Route::get('/userProfile', 'UserProfileController@index')->name('userProfile');
 
